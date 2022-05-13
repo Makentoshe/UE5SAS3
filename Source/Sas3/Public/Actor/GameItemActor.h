c20057a99b56f363655496b433daea9123570019
makentoshe <mkliverout@gmail.com>
2022-05-13 14:54:02 +0300

    feat: add basic game item actor class

---
+++ Source/Sas3/Public/Actor/GameItemActor.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include <Sas3/Public/Structure/FGameItemStructure.h>
+#include "GameItemActor.generated.h"
+
+/*
+ * Most based game item that can be placed on the scene.
+ */
+UCLASS(BlueprintType)
+class SAS3_API AGameItemActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGameItemActor();
+
+public:
+	// Contains general info about item
+	UPROPERTY(EditAnywhere, AdvancedDisplay, BlueprintReadOnly, Category = "Item info")
+	FGameItemStructure GameItem;
+};
