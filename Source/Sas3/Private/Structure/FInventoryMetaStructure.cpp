71554c031c623f6ef76a6bc8ecf5138973193956
makentoshe <mkliverout@gmail.com>
2022-05-17 14:37:33 +0300

    refactor: replace BP InventoryMetaStructure with CPP InventoryMetaStructure

---
+++ Source/Sas3/Private/Structure/FInventoryMetaStructure.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Structure/FInventoryMetaStructure.h"
+
+FInventoryMetaStructure::FInventoryMetaStructure()
+{
+	this->StackSize = 10;
+	this->ItemCount = 0;
+}
---
+++ Source/Sas3/Public/Structure/FInventoryMetaStructure.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "FInventoryMetaStructure.generated.h"
+
+/**
+ * Meta structure works with GameItemStructure that allows 
+ * to understand inventory preferences for this item.
+ */
+USTRUCT(BlueprintType)
+struct SAS3_API FInventoryMetaStructure: public FTableRowBase
+{
+	GENERATED_BODY()
+
+	// Contains how many items is in the current instance
+	// Note: uint32 isn't supported by blueprint, so be careful about negative values
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 ItemCount;
+
+	// How many items might be in one stack. Each new stack consumes an inventory slot
+	// Note: uint32 isn't supported by blueprint, so be careful about negative values
+	// Note: if we don't want item to be stackable, this value should be equal 1
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 StackSize;
+
+	FInventoryMetaStructure();
+};
