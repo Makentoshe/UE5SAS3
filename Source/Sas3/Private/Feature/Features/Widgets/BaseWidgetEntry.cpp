8ce2e91d787efa4b7e83f3fb1d3fd208e62bc013
makentoshe <mkliverout@gmail.com>
2022-06-02 20:38:40 +0300

    refactor: replace WBP_BaseWidget_Entry with cpp UBaseWidget

---
+++ Source/Sas3/Private/Feature/Features/Widgets/BaseWidgetEntry.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Feature/Features/Widgets/BaseWidgetEntry.h"
+
---
+++ Source/Sas3/Public/Feature/Feature_CharacterHud/Widgets/CharacterHudObtainerWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacterHudObtainerWidget.generated.h"
+
+/**
+ * Widget that displays obtained items on the screen during gameplay
+ */
+UCLASS(BlueprintType, Abstract)
+class SAS3_API UCharacterHudObtainerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
