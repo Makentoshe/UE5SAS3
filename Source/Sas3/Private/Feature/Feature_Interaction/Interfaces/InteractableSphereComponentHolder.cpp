fbb2f28b36f77ddc12c5b14cb84959a4a52be2a1
Khvostov Maksim <mkliverout@gmail.com>
2022-06-05 00:27:03 +0300

    feat: implement interface allows to understand is actor interactable

---
+++ Source/Sas3/Private/Feature/Feature_Interaction/Interfaces/InteractableSphereComponentHolder.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Feature/Feature_Interaction/Interfaces/InteractableSphereComponentHolder.h"
+
+// Add default functionality here for any IInteractableSphereComponentHolder functions that are not pure virtual.
