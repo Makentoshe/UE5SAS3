f4112c1a45d720616fd2d1a4d1c6f230054f0d6d
Khvostov Maksim <mkliverout@gmail.com>
2022-06-05 00:33:14 +0300

    feat: implement interface for typesafe access to the InventorableActorComponent

---
+++ Source/Sas3/Private/Feature/Feature_Inventory/Interfaces/InventorableActorComponentHolder.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Feature/Feature_Inventory/Interfaces/InventorableActorComponentHolder.h"
+
+// Add default functionality here for any IInventorableActorComponentHolder functions that are not pure virtual.
