f846f89eb66ee6a5bf5f9dfc139c1755ff9557df
Khvostov Maksim <mkliverout@gmail.com>
2022-06-13 08:22:42 +0300

    feat: remove unused CharacterHudSubsystem

--- Source/Sas3/Private/Feature/Feature_CharacterHud/Subsystems/CharacterHudSubsystem.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Feature/Feature_CharacterHud/Subsystems/CharacterHudSubsystem.h"
-
-void UCharacterHudSubsystem::Initialize(FSubsystemCollectionBase& Collection)
-{
-
-}
