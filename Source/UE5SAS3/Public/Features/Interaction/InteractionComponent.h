09a788d009aec277c74838e0c9b332de2368c93a
Makentoshe <mkliverout@gmail.com>
2023-03-04 21:04:42 +0200

    Create base cpp and UE InteractionComponent class

---
+++ Source/UE5SAS3/Public/Features/Interaction/InteractionComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "InteractionComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class UE5SAS3_API UInteractionComponent : public USphereComponent
+{
+	GENERATED_BODY()
+	
+};
