0d0dbe7aeec7f32d0ca06e29af51f7236d278d0e
Makentoshe <mkliverout@gmail.com>
2023-03-04 21:15:47 +0200

    Implement base cpp and UE InteractionComponent class

---
+++ Source/UE5SAS3/Public/Features/Interaction/InteractorComponent.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InteractorComponent.generated.h"
+
+
+UCLASS(BlueprintType, Blueprintable)
+class UE5SAS3_API UInteractorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+};
