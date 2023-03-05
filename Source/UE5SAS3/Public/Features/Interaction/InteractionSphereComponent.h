1724921d06ad2a841d5b99e9c2317ff61a0410e0
Makentoshe <mkliverout@gmail.com>
2023-03-05 16:52:22 +0200

    Add Base cpp classes for InteractionComponent and InteractorComponent

---
+++ Source/UE5SAS3/Public/Features/Interaction/InteractionSphereComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Features/Feature/Component/UE5SASSphereComponent.h"
+#include "InteractionSphereComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class UE5SAS3_API UInteractionSphereComponent : public UUE5SASSphereComponent
+{
+	GENERATED_BODY()
+	
+};
