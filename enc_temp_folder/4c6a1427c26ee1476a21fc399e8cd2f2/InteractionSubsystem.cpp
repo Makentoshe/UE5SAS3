bbd544fa6e3e8e3ba7a11eaa0310cdfc8cd71894
Makentoshe <mkliverout@gmail.com>
2023-03-12 12:58:57 +0200

    Create empty InteractionSubsystem

---
+++ enc_temp_folder/4c6a1427c26ee1476a21fc399e8cd2f2/InteractionSubsystem.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Features/Interaction/InteractionSubsystem.h"
+
+void UInteractionSubsystem::AddInteractionToInteractor(const TScriptInterface<IInteractionComponent>& InteractionComponent, const TScriptInterface<IInteractorComponent>& InteractorComponent)
+{
+	// Add InteractionComponent to Interactor
+	InteractorComponent->Execute_AddInteractionComponent(InteractorComponent.GetObject(), InteractionComponent);
+
+	//// Invoke InteractionComponent.OnInteractionComponentAvailable
+//InteractionComponent.GetInterface()->Execute_OnInteractionComponentAvailable(InteractionComponent.GetObject());
+}
+
+void UInteractionSubsystem::RemoveInteractionFromInteractor(const TScriptInterface<IInteractionComponent>& InteractionComponent, const TScriptInterface<IInteractorComponent>& InteractorComponent)
+{
+
+}
